725edf2acb12fcb9c33f814eb4717c98d32babbf
Gavin Lai <gavinlaimisc@gmail.com>
2024-10-24 16:26:04 -0700

    cloud hover

---
+++ script.js
@@ -0,0 +1,53 @@
+function onHover_origin()
+{
+    document.getElementById("origin_image").src="Images/blue_cloud.png";
+}
+function offHover_origin()
+{
+    document.getElementById("origin_image").src="Images/cloud.png";
+}
+
+function onHover_school()
+{
+    document.getElementById("school_image").src="Images/blue_cloud.png";
+}
+function offHover_school()
+{
+    document.getElementById("school_image").src="Images/cloud.png";
+}
+
+function onHover_skills()
+{
+    document.getElementById("skills_image").src="Images/blue_cloud.png";
+}
+function offHover_skills()
+{
+    document.getElementById("skills_image").src="Images/cloud.png";
+}
+
+function onHover_projects()
+{
+    document.getElementById("projects_image").src="Images/blue_cloud.png";
+}
+function offHover_projects()
+{
+    document.getElementById("projects_image").src="Images/cloud.png";
+}
+
+function onHover_exp()
+{
+    document.getElementById("exp_image").src="Images/blue_cloud.png";
+}
+function offHover_exp()
+{
+    document.getElementById("exp_image").src="Images/cloud.png";
+}
+
+function onHover_hobbies()
+{
+    document.getElementById("hobbies_image").src="Images/blue_cloud.png";
+}
+function offHover_hobbies()
+{
+    document.getElementById("hobbies_image").src="Images/cloud.png";
+}
